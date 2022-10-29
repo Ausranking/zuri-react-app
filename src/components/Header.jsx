6b53d749006cb8a67b0f9338a366e64005b5ee89
Austin Nze <austinnze1000@gmail.com>
2022-10-30 00:01:54 +0100

    updated profile id

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,15 @@ import "./header.css"
 const Header = () => {
   return (
     <header className="profile-section">
-      <div class="icon">
+      <div className="icon">
         <img src="images/_Avatar share button.png" id="web-icon" alt="desktop share button" />
         <img src="images/share-button.png" id="mob-icon" alt="mobile share button" />
       </div>
 
-      <div id="profile-img">
-        <img src="images/IMG_20220327_131117_728 (2).jpg" id="profile_img_one" alt="Profile" />
-        <img src="images/Device=Web, State=Hover.png" id="profile_img_two" hidden alt="profile" />
+      <div className="profile-img">
+        <img src="images/IMG_20220327_131117_728 (2).jpg" id="profile_img" alt="Profile" />
       </div>
-      <h1 id="twitter">King Austin</h1>
+      <h1 id="twitter">gAustin</h1>
       <h2 id="slack" hidden>Austin Nze</h2>
     </header>
 
